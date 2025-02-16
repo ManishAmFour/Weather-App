7f29e5263904ab86dc86bbe63a75e6494f3d158b
ManishAmFour <ManishTewatia3020@gmail.com>
2025-02-16 23:40:10 +0530

    Made a basic weather app nothing special

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { use, useState } from "react";
+import WebApi from "./weatherApi.jsx";
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setValue] = useState("");
+  const [isSent, setIsSent] = useState(false);
+  const [weatherData, setWeatherData] = useState(false);
+
+  function inputchange(e) {
+    setValue(e.target.value);
+    setIsSent(false);
+  }
+
+  function sent() {
+    setIsSent(true);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="input-forms">
+        <input className="input-bar" onChange={inputchange} value={message} />
+        <button className="input-button" onClick={sent}>Enter</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <WebApi
+        setWeatherData={setWeatherData}
+        function={setIsSent}
+        sentValue={isSent}
+        message={message}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/weatherApi.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import "./weatherApi.css";
+function WebApi(props) {
+  const [displayValue, setDisplayValue] = useState('');
+  const [weatherValue, setWeatherValue] = useState("");
+  const [requestStatus, setRequestStatus] = useState();
+  let PrevValue = displayValue;
+  if (props.sentValue && PrevValue !== props.message) {
+    setDisplayValue(props.message);
+    PrevValue = props.message;
+  }
+
+  useEffect(() => {
+    if (displayValue !== "") {
+      setRequestStatus("pending");
+      fetch(
+        `http://api.weatherapi.com/v1/current.json?key=a0095c765c3146b99a7194304251502&q=${displayValue}`
+      )
+        .then((result) => {
+          return result.json();
+        })
+        .then((response) => {
+          setTimeout(() => {
+            setRequestStatus("fullfilled");
+
+            setWeatherValue({
+              location: response.location.country,
+              city: response.location.name,
+              region: response.location.region,
+              temp: response.current.temp_c,
+            });
+          }, 1000);
+        });
+    }
+  }, [displayValue]);
+
+  if (requestStatus === "pending") {
+    return <p>Loading</p>;
+  } else if (requestStatus === "fullfilled") {
+    return (
+      <div className="weather-container">
+        <p>Name of the city:- {weatherValue.city}</p>
+        <p>Temperature:- {weatherValue.temp} Celsius</p>
+        <p>region:- {weatherValue.region}</p>
+        <p>Country:- {weatherValue.location}</p>
+      </div>
+    );
+  }
+}
+
+export default WebApi;
